bea4f41daa44a1387b73a182c8653b4eea9bbcbe
james.lewis <james@dynamiccarrots.co.uk>
2023-03-20 11:30:16 +0000

    Push live

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,90 +3,18 @@ import Image from 'next/image'
 import {Poppins} from 'next/font/google'
 import styles from '@/styles/Home.module.css'
 import Script from "next/script";
-import {useEffect, useMemo, useState} from "react";
-import {useRouter} from "next/router";
 
 const poppins = Poppins({weight: ['300', '600'], subsets: []});
 
 export default function Home(props) {
-    const router = useRouter();
-
-    const getSiteConfig = () => {
-
-        let config = {
-            affiliate: props.affiliate,
-            geo: null,
-            logo: null,
-            alt: null,
-            link: null
-        }
-
-        switch (props.affiliate) {
-            case '18779': // This is the slush account
-                config.geo = 'UK';
-                break;
-            case '19304':
-                config.logo = '/logos/cashback.png';
-                config.geo = 'UK';
-                config.link = 'https://cashback.co.uk/';
-                break;
-            case '18920':
-                config.logo = '/logos/paid-surveys.png';
-                config.geo = 'UK';
-                config.link = 'https://paidsurveys.uk.com/';
-                break;
-            case '19200' || '18915':
-                config.logo = '/logos/20-cogs.png';
-                config.geo = 'UK';
-                config.link = 'https://20cogs.co.uk/';
-                break;
-            case '18685':
-                config.logo = '/logos/global-survey-hub.png';
-                config.geo = 'US';
-                config.link = 'https://www.globalsurveyhub.com/';
-                break;
-            case '18691':
-                config.logo = '/logos/product-testing-usa.png';
-                config.geo = 'US';
-                config.link = 'https://producttestingusa.com/';
-                break;
-            case '18824':
-                config.logo = '/logos/us-product-testing.png';
-                config.geo = 'US';
-                config.link = 'https://usproducttesting.com/';
-                break;
-        }
-
-        console.log(config);
-
-
-        return config;
-    }
-
-    const siteConfig = useMemo(() => {
-        return getSiteConfig()
-    }, [router?.query?.affiliate])
 
     const renderAdzuki = () => {
-        console.log(siteConfig.geo)
-        if (!siteConfig.geo) return null;
+        if (!props.siteConfig.geo) return null;
 
         return (
             <>
-                <Script id="chadzuki" strategy="lazyOnload">
-                    {`
-                ;(function(a,d,z,u,k,i){
-                    i='https://client.getadzuki.com/adzuki-client';d=document;z=d.getElementsByTagName('head')[0];u=d.createElement('script');
-                    k="noModule" in u;u.async=true;u.src=k?i+'.module.js':i+'.js';z.appendChild(u);k=window;
-                    (k.adsbyadzuki=k.adsbyadzuki||[]).push(['init', a])
-                })
-                ({
-                    geo: "${siteConfig.geo}",
-                })
-              `}
-                </Script>
-
-                <div data-adzuki-id={siteConfig.affiliate} data-adzuki-ads='10'></div>
+                <h2 className={styles.offersHeaderText}>Other offers you might like </h2>
+                <div data-adzuki-id={props.siteConfig.affiliate} data-adzuki-ads='10' data-adzuki-top-tags={props.siteConfig.tag}></div>
             </>
         )
     }
@@ -103,15 +31,28 @@ export default function Home(props) {
                     font-family: ${poppins.style.fontFamily};
                   }
                 `}</style>
-            </Head>
 
+            </Head>
+            <Script
+                id="chadzuki2"
+                strategy="beforeInteractive"
+                dangerouslySetInnerHTML={{
+                    __html: `;(function(a,d,z,u,k,i){
+i='https://client.getadzuki.com/adzuki-client';d=document;z=d.getElementsByTagName('head')[0];u=d.createElement('script');
+k="noModule" in u;u.async=true;u.src=k?i+'.module.js':i+'.js';z.appendChild(u);k=window;
+d.addEventListener('DOMContentLoaded',function(){(k.adsbyadzuki=k.adsbyadzuki||[]).push(['init', a])})})({
+  geo: '${props.siteConfig.geo}',
+  reference : 'extrareward4you'
+})`,
+                }}
+            />
 
             <div className={styles.body}>
                 <div className={styles.header}>
-                    {(siteConfig.logo) ?
+                    {(props.siteConfig.logo) ?
                         <Image
                             className={styles.logo}
-                            src={siteConfig.logo}
+                            src={props.siteConfig.logo}
                             alt="Next.js Logo"
                             width={600}
                             height={200}
@@ -122,8 +63,10 @@ export default function Home(props) {
 
                 </div>
                 <main className={styles.main}>
-                    <h2 className={styles.offersHeaderText}>Other offers you might like </h2>
                     {renderAdzuki()}
+                    {(props.siteConfig.link) ? <a href={props.siteConfig.link} className={styles.button}>
+                        Return To Site
+                    </a> : null }
                 </main>
             </div>
         </>
@@ -131,7 +74,66 @@ export default function Home(props) {
 }
 
 export function getServerSideProps(context) {
+    const getSiteConfig = () => {
+
+        let config = {
+            affiliate: (context?.query?.affiliate) ?? null,
+            geo: null,
+            logo: null,
+            alt: null,
+            link: null,
+            tag: null
+        }
+
+        switch (config.affiliate) {
+            case '19304':
+                config.logo = '/logos/cashback.png';
+                config.geo = 'UK';
+                config.link = 'https://cashback.co.uk/';
+                config.tag = 'cashback';
+                break;
+            case '18920':
+                config.logo = '/logos/paid-surveys.png';
+                config.geo = 'UK';
+                config.link = 'https://paidsurveys.uk.com/';
+                config.tag = 'cashback';
+                break;
+            case '19200' || '18915':
+                config.logo = '/logos/20-cogs.png';
+                config.geo = 'UK';
+                config.link = 'https://20cogs.co.uk/';
+                config.tag = '20cogs';
+                break;
+            case '18685':
+                config.logo = '/logos/global-survey-hub.png';
+                config.geo = 'US';
+                config.link = 'https://www.globalsurveyhub.com/';
+                config.tag = 'coreg';
+                break;
+            case '18691':
+                config.logo = '/logos/product-testing-usa.png';
+                config.geo = 'US';
+                config.link = 'https://producttestingusa.com/';
+                config.tag = 'coreg';
+                break;
+            case '18824':
+                config.logo = '/logos/us-product-testing.png';
+                config.geo = 'US';
+                config.link = 'https://usproducttesting.com/';
+                config.tag = 'coreg';
+                break;
+        }
+
+        config.tag = (context?.query?.tag) ?? config.tag
+        return config;
+    }
+
+
     return {
-        props: {affiliate: (context?.query?.affiliate) ?? null},
+        props: {
+            affiliate: (context?.query?.affiliate) ?? null,
+            tag: (context?.query?.tag) ?? null,
+            siteConfig: getSiteConfig()
+        },
     };
 }
